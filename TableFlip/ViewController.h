35ba5d22212d6dfebecbc4746708979c12ae00ca
Paul Stefan Ort <pso@pso.io>
2016-03-03 14:59:24 -0500

    Initial commit: basic UITableView with one UITableViewCell

---
+++ TableFlip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableFlip
+//
+//  Created by Paul Stefan Ort on 3/3/16.
+//  Copyright © 2016 pso.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+
+@end
+
---
+++ TableFlip/ViewController.m
@@ -0,0 +1,95 @@
+//
+//  ViewController.m
+//  TableFlip
+//
+//  Created by Paul Stefan Ort on 3/3/16.
+//  Copyright © 2016 pso.io. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () {
+    UITableView *_tableView;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    [self setupViews];
+}
+
+- (void)setupViews {
+    self.view.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    _tableView = [UITableView new];
+    _tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    _tableView.backgroundColor = [UIColor clearColor];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    _tableView.estimatedRowHeight = 66.0f;
+    [_tableView reloadData];
+    
+    _tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addSubview:_tableView];
+    
+    NSLayoutConstraint *topConstraint = [NSLayoutConstraint constraintWithItem:_tableView
+                                                                     attribute:NSLayoutAttributeTop
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeTop
+                                                                    multiplier:1.0f
+                                                                      constant:0.0f];
+    NSLayoutConstraint *bottomConstraint = [NSLayoutConstraint constraintWithItem:_tableView
+                                                                        attribute:NSLayoutAttributeBottom
+                                                                        relatedBy:NSLayoutRelationEqual
+                                                                           toItem:self.view
+                                                                        attribute:NSLayoutAttributeBottom
+                                                                       multiplier:1.0f
+                                                                         constant:0.0f];
+    NSLayoutConstraint *leadingConstraint = [NSLayoutConstraint constraintWithItem:_tableView
+                                                                         attribute:NSLayoutAttributeLeadingMargin
+                                                                         relatedBy:NSLayoutRelationEqual
+                                                                            toItem:self.view
+                                                                         attribute:NSLayoutAttributeLeading
+                                                                        multiplier:1.0f
+                                                                          constant:0.0f];
+    NSLayoutConstraint *trailingConstraint = [NSLayoutConstraint constraintWithItem:_tableView
+                                                                          attribute:NSLayoutAttributeTrailingMargin
+                                                                          relatedBy:NSLayoutRelationEqual
+                                                                             toItem:self.view
+                                                                          attribute:NSLayoutAttributeTrailing
+                                                                         multiplier:1.0f
+                                                                           constant:0.0f];
+    [self.view addConstraint:topConstraint];
+    [self.view addConstraint:bottomConstraint];
+    [self.view addConstraint:leadingConstraint];
+    [self.view addConstraint:trailingConstraint];
+}
+
+# pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"cellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %ld", indexPath.row];
+    return cell;
+}
+
+# pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
