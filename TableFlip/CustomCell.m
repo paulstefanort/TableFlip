d603ee9ba2f2c627317efc235e8dd0f7870bfde3
Paul Stefan Ort <pso@pso.io>
2016-03-03 15:38:05 -0500

    Add card to custom cell

--- TableFlip/CustomCell.m
+++ TableFlip/CustomCell.m
@@ -9,6 +9,7 @@
 #import "CustomCell.h"
 
 @interface CustomCell() {
+    UIView *_card;
     UILabel *_commentLabel;
     UILabel *_rankLabel;
 }
@@ -21,6 +22,13 @@
     if (self) {
         self.translatesAutoresizingMaskIntoConstraints = NO;
         // no self.contentView.translatesAutoresizingMaskIntoConstraints = NO;
+        self.backgroundColor = [UIColor clearColor];
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+        _card = [UIView new];
+        _card.backgroundColor = [UIColor whiteColor];
+        _card.layer.cornerRadius = 3.0f;
+        _card.translatesAutoresizingMaskIntoConstraints = NO;
 
         _commentLabel = [UILabel new];
         _commentLabel.translatesAutoresizingMaskIntoConstraints = NO;
@@ -32,20 +40,33 @@
         _rankLabel.backgroundColor = [UIColor darkGrayColor];
         _rankLabel.textColor = [UIColor whiteColor];
         
-        [self.contentView addSubview:_commentLabel];
-        [self.contentView addSubview:_rankLabel];
+        [_card addSubview:_commentLabel];
+        [_card addSubview:_rankLabel];
+        [self.contentView addSubview:_card];
         
-        NSArray *horizontalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[_commentLabel]-10-[_rankLabel]-20-|"
+        NSArray *horizontalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[_commentLabel]-10-[_rankLabel]-10-|"
                                                                                  options:NSLayoutFormatAlignAllTop
                                                                                  metrics:nil
                                                                                    views:NSDictionaryOfVariableBindings(_commentLabel, _rankLabel)];
-        [self.contentView addConstraints:horizontalConstraints];
+        [_card addConstraints:horizontalConstraints];
         
         NSArray *verticalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-10-[_commentLabel(60)]-10-|"
                                                                                options:0
                                                                                metrics:nil
                                                                                  views:NSDictionaryOfVariableBindings(_commentLabel)];
-        [self.contentView addConstraints:verticalConstraints];
+        [_card addConstraints:verticalConstraints];
+        
+        NSArray *cardHorizontalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[_card]-20-|"
+                                                                                     options:0
+                                                                                     metrics:0
+                                                                                       views:NSDictionaryOfVariableBindings(_card)];
+        [self.contentView addConstraints:cardHorizontalConstraints];
+        
+        NSArray *cardVerticalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-10-[_card]-10-|"
+                                                                                   options:0
+                                                                                   metrics:0
+                                                                                     views:NSDictionaryOfVariableBindings(_card)];
+        [self.contentView addConstraints:cardVerticalConstraints];
     }
     return self;
 }
