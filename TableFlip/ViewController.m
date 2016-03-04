23d3f3e149683576704b7a187e8a9f257858f49f
Paul Stefan Ort <pso@pso.io>
2016-03-04 10:32:48 -0500

    Rearrange ViewController, putting view setup at the end

--- TableFlip/ViewController.m
+++ TableFlip/ViewController.m
@@ -22,6 +22,32 @@
     [self setupViewsAndConstraints];
 }
 
+# pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"cellIdentifier";
+    CustomCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    NSString *value;
+    if (indexPath.row % 2 == 0) {
+        value = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia mollis purus et efficitur. Donec ac mi at urna semper mollis. Ut hendrerit rutrum lorem vel blandit. Ut accumsan ex vel lectus consequat, elementum lacinia neque tempor. Integer nec massa sit amet ante interdum dapibus.";
+    } else {
+        value = @"Some comment";
+    }
+    [cell presentValue:value];
+    return cell;
+}
+
+# pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+# pragma mark - View Setup
+
 - (void)setupViewsAndConstraints {
     [self setupTableView];
     [self setupConstraints];
@@ -54,30 +80,6 @@
                                options:0
                                metrics:0
                                views:viewDictionary]];
-}
-
-# pragma mark - UITableViewDataSource
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *cellIdentifier = @"cellIdentifier";
-    CustomCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    NSString *value;
-    if (indexPath.row % 2 == 0) {
-        value = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia mollis purus et efficitur. Donec ac mi at urna semper mollis. Ut hendrerit rutrum lorem vel blandit. Ut accumsan ex vel lectus consequat, elementum lacinia neque tempor. Integer nec massa sit amet ante interdum dapibus.";
-    } else {
-        value = @"Some comment";
-    }
-    [cell presentValue:value];
-    return cell;
-}
-
-# pragma mark - UITableViewDelegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
